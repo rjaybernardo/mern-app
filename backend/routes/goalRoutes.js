5ae1b16da2f263a8fc028d244eeea6cbaefa1285
Raphael Jay Bernardo <raphaeljaybernardo@gmail.com>
2022-10-11 06:45:33 -0700

    cleaner routes

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const router = express.Router()
-const { getGoals,setGoals,updateGoals,deleteGoals } = require('../controllers/goalController')
+const {
+  getGoals,
+  setGoals,
+  updateGoals,
+  deleteGoals,
+} = require('../controllers/goalController')
 
-router.get('/', getGoals)
-router.post('/', setGoals)
-router.put('/:id', updateGoals
-router.delete('/:id', deleteGoals)
+router.route('/').get(getGoals).post(setGoals)
+router.route('./:id').put(updateGoals).delete(deleteGoals)
 
 module.exports = router
