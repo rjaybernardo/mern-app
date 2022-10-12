9e336dd406a5f3e40a80b552445778378daf611c
Raphael Jay Bernardo <raphaeljaybernardo@gmail.com>
2022-10-12 00:12:51 -0700

    add json web token

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -38,6 +38,7 @@ const registerUser = asyncHandler(async (req, res) => {
       _id: user.id,
       name: user.name,
       email: user.email,
+      token: generateToken(user._id),
     })
   } else {
     res.status(400)
@@ -59,6 +60,7 @@ const loginUser = asyncHandler(async (req, res) => {
       _id: user.id,
       name: user.name,
       email: user.email,
+      token: generateToken(user._id),
     })
   } else {
     res.status(400)
